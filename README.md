4c2e37c0ef2b64a13badb1aaede7dd156d82dd81
RishadYamnoor <rishad.yamnoor@gmail.com>
2019-02-28 23:22:37 +0530

    Working minimal

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 ### This one of the projects for Udacity Frontend Nanodegree Program
 
+The app shows the most interesting and popular places in Bengaluru, Silicon Valley of India.
+
+I have implemented KnockoutJS library for MVVM pattern to keep the data, view and connection between as simple as possible.
+
+Frameworks and APIs used
+
+Knockout JS
+Google Maps
+Foursquare
+JQuery
+Bootstrap
+
+
+### How to view this App
+
+Download the source code and open index.html in your browser of choice.
+app.js is the heart of the application.
---
+++ js/app.js
@@ -0,0 +1,162 @@
+
+//Interesting places in Bengaluru
+//////////////////   Model  \\\\\\\\\\\\\\\\\\\\
+let places = [
+    {
+        title: 'Bangalore Palace',
+        location: {
+            lat: 12.595470,
+            lng: 77.353157
+        }
+    },{
+        title: 'Lal Bagh Botanical Garden',
+        location: {
+            lat: 12.948228,
+            lng: 77.586002
+        }
+    },{
+        title: 'Nandi Hills',
+        location: {
+            lat: 0.104680,
+            lng: 35.183159
+        }
+    },{
+        title: 'Cubbon Park',
+        location: {
+            lat: 12.976347,
+            lng: 77.592926
+        }
+    },{
+        title: 'Vidhana Soudha',
+        location: {
+            lat: 12.982030,
+            lng: 77.593536
+        }
+    },{
+        title: 'Bannerghatta National Park',
+        location: {
+            lat: 12.800285,
+            lng: 77.577049
+        },
+    },{
+        title: 'Jawaharlal Nehru Planetarium',
+        location: {
+            lat: 12.984847,
+            lng: 77.589619
+        }
+    },{
+        title: 'Sankey Tank',
+        location: {
+            lat: 13.009888,
+            lng: 77.574004
+        }
+    }
+];
+
+/////////////////   Initialize the map   \\\\\\\\\\\\\\\\\\\\
+
+ 
+
+
+        
+///////////////////   ViewModel   \\\\\\\\\\\\\\\\\\\\
+
+ViewModel = () => {
+    let self = this;
+    self.locations = ko.observableArray(); //array of locations
+    self.search = ko.observable(''); // query
+    self.markers = [];
+
+       
+    
+   // Filtering
+   self.searchLocations = ko.computed(() => {
+    let q = this.filter().toLowerCase();
+
+        for (var i = 0; i < self.places().length; i++) {
+            if (self.places()[i].name().toLowerCase().indexOf(q) > -1) {
+                for (var j = 0; j < markers.length; j++) {
+                    if (self.places()[i].name() == markers[j].title) {
+                        markers[j].setVisible(true);
+                    }
+                }
+            } else {
+                for (var k = 0; k < markers.length; k++) {
+                    if (self.places()[i].name() == markers[k].title) {
+                        markers[k].setVisible(false);
+                    }
+                }
+            }
+        }
+        if (!q) {
+        return self.places();
+        } else {
+            return ko.utils.arrayFilter(this.spaceList(), (item) => {
+                return item.name.toLowerCase().indexOf(q) > -1; 
+            });
+        }
+    });
+
+    let largeInfoWindow = new google.maps.InfoWindow();
+
+// when marker is clicked, the infowindow is populated
+populateInfoWindow = (marker, infowindow) => {
+    if(infowindow.marker != marker) {
+        infowindow.marker = marker;
+        // infowindow.addListener('clickclose', () => infowindow.marker = null); 
+
+        //Foursquare API request
+        let client_ID = 'FDGCRKRLYS42R3NUFTQTFB5PFCQ1YP0GOHBTBBXWYWEJM2QD';
+        let client_secret = '311TMOJ0BLVKHZ5PVU4GLLKV4M5N33DNY0GBHZ3YY5Z2DHWB';
+        let loc = marker.position.lat() + ',' + marker.position.lng();
+
+        let foursquareURL = 'https://api.foursquare.com/v2/venues/search?11=' +
+                            loc +
+                            '&client_id=' + client_ID + 
+                            '&client_secret=' + client_secret + 
+                            '&v=20161129&m=foursquare';
+            $.ajax({
+                url: foursquareURL,
+                dataType: 'jsonp',
+                async: false,
+                success: (data) => {
+                    let details = {id: "", name: "", address: ""};
+                    details.name = data.response.venues[0].name;
+                    details.id = data.response.venues[0].id;
+                    details.address = data.response.venues[0].location.formattedAddress;
+
+                    infowindow.setContent('<div>' + details.name + '</div>' + '<div>Address:' + details.address + '</div>');
+
+                    bounceAnimation(marker);
+                    infowindow.open(map, marker);
+
+                error: () => {
+                    infowindow.setContent('<div>Unable to get the data</div>');
+                }
+            }
+        });
+
+        infowindow.addListener('closeclick', () => { infowindow.marker = null; }); //to clear the marker
+    }
+}
+}
+
+bounceAnimation = (marker) => {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(() => { marker.setAnimation(null); }, 700);
+    }
+}
+
+
+
+
+
+
+
+
+googleMapError = () => {
+    $('#map').html('Google map could not load, try again.')
+}
